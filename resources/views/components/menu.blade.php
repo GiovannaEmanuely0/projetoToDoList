55cb5d9ec87aacc5ca94d2742e54c6730157990a
Giovanna Emanuely <giovannaEmma16@gmail.com>
2026-04-23 11:46:26 -0300

    Update menu.blade.php

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,13 +1,25 @@
-<div class="menuLateral">
-    <div class="divInternaMenuIcon"><span class="material-symbols-outlined">menu</span></div>
-                <!-- clicar no icon de menu e retornar a tela -->
-    <div class="divInternaMenu">
-        <p>Accont</p>
-        <p>Accont</p>
-        <p>Accont</p>
-        <p>Accont</p>
-        <p>Accont</p>
-        <p>Accont</p>
+<div class="sidebar">
+    <div class="div_sidebar">
+        <div class="sidebar_logo"><h1>logo</h1><p>descricao</p></div>
+        <h1>Hoje</h1>
+        <h1>Em processo</h1>
+        <h1>Completas</h1>
+    </div>
+    <div class="div_sidebar">
+        <h1>Categorias</h1>
+        <input type="submit" value="+">
+        <div>
+            <ul>
+                <li>Exemplo 1</li>
+                <li>Exemplo 2</li>
+                <li>Exemplo 3</li>
+                <li>Exemplo 4</li>
+            </ul>
+        </div>
+    </div>
+    <div class="div_sidebar">
+        <div class="sidebar_grafico">grafico</div>
+        <div class="sidebar_user">usuario</div>
     </div>
 </div>
 <link rel="stylesheet" href="{{url('css/menu.css')}}">
